999267c243da331ba32a2a3e2c405271c5fce845
최병민(01045842518) <byung803@gmail.com>
2017-01-10 19:35:16 +0900

    Until Section 8-2

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,1 +1,18 @@
 // Index.android.js - place code in here for An Android!
+
+//1 Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+//2 Create a component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+  );
+
+//3 Render it to the device
+AppRegistry.registerComponent('albums', () => App);
