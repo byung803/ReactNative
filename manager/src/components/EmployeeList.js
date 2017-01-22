c7bb19d799f526cedbad47066aaa66b1d1c65099
최병민(01045842518) <byung803@gmail.com>
2017-01-22 14:41:36 +0900

    until Section 18_ 144 before fetching

---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { View, Text } from 'react-native';
+import { employeesFetch } from '../actions';
+
+
+class EmployeeList extends Component {
+  componentWillMount() {
+  //  this.props.employeesFetch();
+  }
+    render() {
+      return (
+        <View>
+          <Text>EmployeeList</Text>
+          <Text>EmployeeList</Text>
+          <Text>EmployeeList</Text>
+          <Text>EmployeeList</Text>
+          <Text>EmployeeList</Text>
+          <Text>EmployeeList</Text>
+          <Text>EmployeeList</Text>
+        </View>
+      );
+    }
+}
+
+export default connect(null, { employeesFetch })(EmployeeList);
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -7,8 +7,8 @@ import {
  } from '../actions/types';
 
 const INITIAL_STATE = {
-  email: '',
-  password: '',
+  email: 'a@a.com',
+  password: '123456',
   user: null,
   error: '',
   loading: false
